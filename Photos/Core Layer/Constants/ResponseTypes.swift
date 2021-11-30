05ca7ebc7481a5354dbbbde663b210ceb440fb4e
Kanan Abilzada <49763751+KananAbilzada@users.noreply.github.com>
2021-11-30 18:29:00 +0400

    Create ResponseTypes.swift

---
+++ Photos/Core Layer/Constants/ResponseTypes.swift
@@ -0,0 +1,10 @@
+//
+//  ResponseTypes.swift
+//  Photos
+//
+//  Created by Kanan Abilzada on 30.11.21.
+//
+
+import Foundation
+
+typealias PhotoGetPhotosResponseType = Result<[PhotoListModel], NetworkError>
