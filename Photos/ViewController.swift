6156003db9b28b88cebbd24afb106ee601d97285
Kanan Abilzada <49763751+KananAbilzada@users.noreply.github.com>
2021-11-30 11:05:33 +0400

    Initial Commit

---
+++ Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Kanan`s Macbook Pro on 11/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
